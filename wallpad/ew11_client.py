e3dcf04c04d56a27679d8bbd89b65b8bfc783452
mason <mason@toss.im>
2023-08-19 10:07:24 +0900

    break 버그 수정

--- wallpad/ew11_client.py
+++ wallpad/ew11_client.py
@@ -13,24 +13,26 @@ received_packets = {}
 async def command_consumer(writer, config: RuntimeConfig):
     while True:
         msg: Command = await config.command_queue.get()
-        _LOGGER.debug(f"Consumed from queue: {msg}")
-        retry_count = 5 if msg.ack else 2
+        _LOGGER.debug(f"Consumed from queue {msg}")
+        retry_count = 2
+        _LOGGER.info(f'Update state {msg.state}')
+        await config.state_queue.put(msg.state)
         for cmd in msg.commands:
+            done = False
             for i in range(retry_count):
-                await config.state_queue.put(msg.state)
-                writer.write(bytes.fromhex(cmd))
-                _LOGGER.info(f'Send the {cmd} on the {i + 1} attempt.')
-                await asyncio.sleep(2)
-                if not msg.ack:
-                    _LOGGER.info(f'No ACK is set, so it will be sent twice.')
-                    await asyncio.sleep(retry_count)
-                    break
-                elif msg.ack in ACK:
-                    _LOGGER.info(f'After 2 sec, receive an ACK {msg.ack} or send the command up to 5 times.')
-                else:
-                    _LOGGER.info(f"it received an ACK {msg.ack}, so we don't retransmit.")
-                    del ACK[msg.ack]
-                    break
+                if not done:
+                    writer.write(bytes.fromhex(cmd))
+                    _LOGGER.info(f'Send the {cmd} on the {i + 1} attempt.')
+                    await asyncio.sleep(2)
+                    if not msg.ack:
+                        _LOGGER.info(f'No ACK is set, so it will be sent twice.')
+                    elif msg.ack in ACK:
+                        _LOGGER.info(f"it received an ACK {msg.ack}, so we don't retransmit.")
+                        del ACK[msg.ack]
+                        _LOGGER.info('set done flag True')
+                        done = True
+                    else:
+                        _LOGGER.info(f'After 2 sec, receive an ACK {msg.ack} or send the command up to 5 times.')
 
         _LOGGER.info(f'{cmd} writer drain')
         await writer.drain()
